5f4a08c1155df7d74154e378b23c0c205b11efb9
sebastianbarkan <86000650+sebastianbarkan@users.noreply.github.com>
2023-05-17 12:56:58 -0400

    add posts and related functionalities

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,15 +6,60 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
-    <h2>register</h2>
-    <form action="/register" method="POST">
+    @auth
+    <p>Congrats logged in</p>
+    <form action="/logout" method="POST">
         @csrf
-        <input name="name" type="text" placeholder="name">
-        <input name="email" type="email" placeholder="email">
-        <input name="password" type="password" placeholder="password">
-        <button>Register</button>
+        <button>Log Out</button>
     </form>
-</div>
+
+    <div>
+        <h2>Create a New Post</h2>
+        <form action="/create-post" method="POST">
+            @csrf
+            <input type="text" name="title" placeholder="post title">
+            <textarea name="body" placeholder="body content..."></textarea>
+            <button>Save Post</button>
+        </form>
+    </div>
+
+    <div>
+        <h2>All Posts</h2>
+        @foreach($posts as $post)
+        <div>
+            <h3>{{$post['title']}} by {{$post->user->name}}</h3>
+            {{$post['body']}}
+            <p><a href="/edit-post/{{$post->id}}">Edit</a></p>
+            <form action="/delete-post/{{$post->id}}" method="POST">
+                @csrf
+                @method('DELETE')
+                <button>Delete</button>
+            </form>
+        </div>
+        @endforeach
+    </div>
+    @else
+    <div>
+        <h2>Login</h2>
+        <form action="/login" method="POST">
+            @csrf
+            <input name="loginname" type="text" placeholder="name"> 
+            <input name="loginpassword" type="password" placeholder="password">
+            <button>Login</button>
+        </form>
+    </div>
+    <div>
+        <h2>register</h2>
+        <form action="/register" method="POST">
+            @csrf
+            <input name="name" type="text" placeholder="name">
+            <input name="email" type="email" placeholder="email">
+            <input name="password" type="password" placeholder="password">
+            <button>Register</button>
+        </form>
+    </div>
+
+    @endauth
+
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +16,22 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $posts = [];
+
+    if (auth()->check()) {
+        $posts = auth()->user()->usersCoolPosts()->latest()->get();
+    };
+
+
+    return view('home', ["posts" => $posts]);
 });
 
 Route::post('/register', [UserController::class, 'register']);
+Route::post('/logout', [UserController::class, 'logout']);
+Route::post('/login', [UserController::class, 'login']);
+
+//Blog post related routes
+Route::post("/create-post", [PostController::class, 'createPost']);
+Route::get("/edit-post/{post}", [PostController::class, "showEditScreen"]);
+Route::put("/edit-post/{post}", [PostController::class, "saveEditChanges"]);
+Route::delete("/delete-post/{post}", [PostController::class, "deletePost"]);
