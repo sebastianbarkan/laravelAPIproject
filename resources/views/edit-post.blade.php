5dff7f3257357e2bad684ad974942e400802b6a2
sebastianbarkan <86000650+sebastianbarkan@users.noreply.github.com>
2023-05-17 13:52:25 -0400

    edit page basic styling

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -3,16 +3,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="{{ url('css/app.css') }}">
+    <title>Laravel Blog</title>
 </head>
 <body>
-    <h1>Edit Post</h1>
-    <form action="/edit-post/{{$post->id}}" method="POST">
-        @csrf
-        @method("PUT")
-        <input type="text" name="title" value="{{$post->title}}">
-        <textarea name="body">{{$post->body}}</textarea>
-        <button>Save Changes</button>
-    </form>
+    <div class="wrapper">
+        <h1>Edit Post</h1>
+        <form action="/edit-post/{{$post->id}}" method="POST" class="edit-post-wrapper">
+            @csrf
+            @method("PUT")
+            <input type="text" name="title" value="{{$post->title}}">
+            <textarea name="body">{{$post->body}}</textarea>
+            <button>Save Changes</button>
+        </form>
+    </div>
 </body>
 </html>
